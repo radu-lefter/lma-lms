ea93f51304a9d6674c6685f81335398d1595980d
raduubraduu <lefter_radu@yahoo.com>
2023-10-18 18:08:12 +0100

    implemented authentication with clerk

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@/components/ui/button'
-import Image from 'next/image'
+
+import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
   return (
     <div>
-    <p className='text-3xl font-medium text-sky-700'>Hello World</p>
-    <Button>Click here</Button>
+    <UserButton afterSignOutUrl='/'/>
+
     </div>
 
   )
