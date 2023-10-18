70796adb7eff167fce86458064e7a398cafaae57
raduubraduu <lefter_radu@yahoo.com>
2023-10-18 19:28:31 +0100

    created a basic navigation

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,12 @@
+const SearchPage = () => {
+  
+    return (
+      <>
+        <div className="px-6 pt-6 md:hidden md:mb-0 block">
+          Search page here
+        </div>
+      </>
+     );
+  }
+   
+  export default SearchPage;
