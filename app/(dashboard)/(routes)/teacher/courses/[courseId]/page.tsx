20d064263d479f550ad60185db288dc8ee394a06
raduubraduu <lefter_radu@yahoo.com>
2023-10-20 14:50:44 +0100

    created course category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -11,6 +11,7 @@ import {
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { CategoryForm } from "./_components/category-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -28,6 +29,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   if (!course) {
     return redirect("/");
   }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
 
   const requiredFields = [
     course.title,
@@ -85,14 +92,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
               initialData={course}
               courseId={course.id}
             />
-            {/* <CategoryForm
+            <CategoryForm
               initialData={course}
               courseId={course.id}
               options={categories.map((category) => ({
                 label: category.name,
                 value: category.id,
               }))}
-            />  */}
+            /> 
           </div>
           <div className="space-y-6">
             <div>
