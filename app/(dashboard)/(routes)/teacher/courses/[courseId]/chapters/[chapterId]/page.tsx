b9ae17d2c2edfd0c2721ed58f64df61c6ac57908
raduubraduu <lefter_radu@yahoo.com>
2023-10-22 18:09:56 +0100

    created chapter access component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,7 +9,7 @@ import { IconBadge } from "@/components/icon-badge";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
-//import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 //import { ChapterVideoForm } from "./_components/chapter-video-form";
 //import { ChapterActions } from "./_components/chapter-actions";
 
@@ -114,11 +114,11 @@ const ChapterIdPage = async ({
                   Access Settings
                 </h2>
               </div>
-              {/* <ChapterAccessForm
+              <ChapterAccessForm
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId}
-              /> */}
+              />
             </div>
           </div>
           <div>
